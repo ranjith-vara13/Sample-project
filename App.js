31e3a30ac3b6bd3517418ff8c841bd7dc74a6f6d
ranjith-vara <vararanjith79@gmail.com>
2024-12-20 15:57:14 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,8 @@
+
+const heading =
+
+
+React.createElement
+("h1", {id : "heading"}, "hello ranjith");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "hello ranjith");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
